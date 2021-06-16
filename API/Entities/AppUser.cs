f885f730a28712f75bcf62fcee45594529f744d5
srisaiomkar <srisaiomkar@gmail.com>
2021-06-16 22:29:06 +0530

    extended entities

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
@@ -6,6 +10,19 @@ namespace API.Entities
          public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NickName { get; set; }
+        public string Bio { get; set; }
+        public DateTime AccountCreated { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Interests { get; set; }
+        public string LookingFor { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+        public int GetAge(){
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
