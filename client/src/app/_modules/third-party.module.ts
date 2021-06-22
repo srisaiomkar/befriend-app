b5aee38f60d0d8bf86a2fa2e5ae385aef83f66b5
srisaiomkar <srisaiomkar@gmail.com>
2021-06-22 12:04:16 +0530

    member detail page

--- client/src/app/_modules/third-party.module.ts
+++ client/src/app/_modules/third-party.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 @NgModule({
@@ -11,11 +12,13 @@ import { ToastrModule, ToastrService } from 'ngx-toastr';
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
-    })
+    }),
+    TabsModule.forRoot()
   ],
   exports:[
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class ThirdPartyModule { }
