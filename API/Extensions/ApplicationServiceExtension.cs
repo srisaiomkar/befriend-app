98b82061d945bb255cac0a6331b97a2a5e5277c4
srisaiomkar <srisaiomkar@gmail.com>
2021-06-30 21:35:24 +0530

    added cloudinary and settings

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -13,6 +13,7 @@ namespace API.Extensions
     public static class ApplicationServiceExtension
     {
         public static void AddApplicationServices(this IServiceCollection services, IConfiguration config){
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService,TokenService>();
             services.AddScoped<IUserRepository,UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
