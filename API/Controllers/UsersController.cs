23e87971e65fba77f34a561b9e88552a4ec3a69c
srisaiomkar <srisaiomkar@gmail.com>
2021-06-30 14:36:01 +0530

    update user BE

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.Data.IRepositories;
@@ -36,6 +37,17 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMembersByNameAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(UpdateMemberDto updateMemberDto){
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user =await  _userRepository.GetUserByNameAsync(username);
+            _mapper.Map(updateMemberDto,user);
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllChangesAsync()) return NoContent();
+            return BadRequest("Updating user failed");
         }
     }
 }
---
+++ API/DTOs/UpdateMemberDto.cs
@@ -0,0 +1,12 @@
+namespace API.DTOs
+{
+    public class UpdateMemberDto
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string NickName { get; set; }
+        public string Bio { get; set; }
+        public string Interests { get; set; }
+        public string LookingFor { get; set; }
+    }
+}
