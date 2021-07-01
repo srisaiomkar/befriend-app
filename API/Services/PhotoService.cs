67eec281786ff1987ba26c26154bab46a68ff2ba
srisaiomkar <srisaiomkar@gmail.com>
2021-07-01 16:13:08 +0530

    added photo service

---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using API.Helpers;
+using API.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace API.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var account = new Account{
+                Cloud = config.Value.CloudName,
+                ApiKey = config.Value.ApiKey,
+                ApiSecret = config.Value.ApiSecret
+            };
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var result = new DeletionResult();
+            var deletionParams = new DeletionParams(publicId);
+            await _cloudinary.DestroyAsync(deletionParams);
+            return result;
+        }
+
+        public async Task<ImageUploadResult> UploadPhotoAsync(IFormFile file)
+        {
+            var uploadresult = new ImageUploadResult();
+            if(file.Length > 0){
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams{
+                    File = new FileDescription(file.FileName,stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
+                };
+                var result = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadresult;
+        }
+    }
+}
